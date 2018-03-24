03a55da78c99326ec154068740576f2d90bd24f1
maszvih <maszvih@freemail.hu>
2018-03-24 18:05:07 +0100

    180324

---
+++ app/src/main/java/com/example/win_10/eurokegel/GameActivity.java
@@ -0,0 +1,15 @@
+package com.example.win_10.eurokegel;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
