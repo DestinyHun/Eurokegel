3802fa1017164499f3cf75ccffc607a6e535567f
maszvih <maszvih@freemail.hu>
2018-03-26 13:34:39 +0200

    180324

--- app/src/main/java/com/example/win_10/eurokegel/Constants.java
+++ app/src/main/java/com/example/win_10/eurokegel/Constants.java
@@ -4,4 +4,7 @@ class Constants {
     static String PlayerOne = "";
     static String PlayerTwo = "";
     static String BeginnerPlayer = "";
+    static int PlayerOnePoints = 0;
+    static int PlayerTwoPoints = 0;
+    static Boolean AddPoint = true;
 }
